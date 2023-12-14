83a3f9f264346ac0989b2ca191fa087c8a098487
Gyasmin Alves <gyasmin.alves@linkapi.com.br>
2023-12-14 01:48:10 -0300

    implementing zod for env variables

--- src/database.ts
+++ src/database.ts
@@ -1,11 +1,12 @@
 import 'dotenv/config'
 
 import { knex as setupKnex, Knex } from 'knex'
+import { env } from './env'
 
 export const databaseConfig: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: process.env.DATABASE_URL || '',
+    filename: env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
